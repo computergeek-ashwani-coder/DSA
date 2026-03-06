f812cab8465dca14b085fa4889d604df88e8fef1
computergeek-ashwani-coder <168176169+computergeek-ashwani-coder@users.noreply.github.com>
2026-03-06 23:42:52 +0530

    Add optimized implementation for Maximum Subarray Sum using O(n) approach

--- ARRAY/Day-3/KadaneAlgo.cpp
+++ ARRAY/Day-3/KadaneAlgo.cpp
@@ -26,10 +26,27 @@ int FindMaximumSubarray(vector<int> &arr){
 
 }
 
+// Using Better Approach Takes:- TimeComplexity: O(n) and spaceComplexity: O(n)
+int FindMaxSubArray(vector<int> &arr){
+    int res = arr[0];
+    int maxending = arr[0];
+
+    // Traverse the array from left to right
+    for(int i= 1 ; i < arr.size(); i++){
+
+        maxending = (maxending + arr[i] > arr[i]) ? maxending + arr[i] : arr[i];
+
+        res = (res > maxending) ? res : maxending;
+    }
+
+    return res;
+} 
+
 
 
 int main(){
     vector<int> arr = {2 , 3, - 8 , 7 , -1 , 2 , 3};
     cout<<FindMaximumSubarray(arr)<<endl;
+    cout<<FindMaxSubArray(arr)<<endl;
     return 0;
 }
